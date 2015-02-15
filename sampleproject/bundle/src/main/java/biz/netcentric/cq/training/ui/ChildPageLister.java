d26704792ba852349a474af4a1d9ad52db935c18
Erik Grijzen <erik.grijzen@netcentric.biz>
2015-02-15 23:13:26 +0100

    Finished Sightly exercises 1 and 2

---
+++ sampleproject/bundle/src/main/java/biz/netcentric/cq/training/ui/ChildPageLister.java
@@ -0,0 +1,41 @@
+package biz.netcentric.cq.training.ui;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.adobe.cq.sightly.WCMUse;
+import com.day.cq.commons.Filter;
+import com.day.cq.wcm.api.Page;
+
+public class ChildPageLister extends WCMUse {
+
+    List<Page> children = new LinkedList<Page>();
+
+    private String filter = "default";
+    
+    @Override
+    public void activate() throws Exception {
+        filter = get("initial", String.class);
+
+        Iterator<Page> it = getCurrentPage().listChildren(new Filter<Page>() {
+
+            @Override
+            public boolean includes(Page page) {
+                return filter == null || page.getTitle() != null && page.getTitle().startsWith(filter);
+            }
+            
+        });
+        while (it.hasNext()) {
+            children.add(it.next());
+        }
+    }
+
+    public List<Page> getChildren() {
+        return children;
+    }
+
+    public String getFilter() {
+        return filter;
+    }
+}
